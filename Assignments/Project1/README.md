18797cabfb13229891fa2bb70ba2ec4f70caa027
cesar-gz <cesarg7@csu.fullerton.edu>
2023-02-26 15:07:00 -0800

    finished algorithm 2 problem with n squared solution.

--- Assignments/Project1/README.md
+++ Assignments/Project1/README.md
@@ -4,3 +4,8 @@
 In the terminal of the algo1.py directory type, `python algo1.py`
 The preset test case will print the results in the terminal
 Change the preset test case to test your own values
+
+### For Algorithm 2:  Matching Group Schedules
+In the terminal of the algo1.py directory type, `python algo2.py`
+The preset test case will print the results in the terminal
+Change the preset test case to test your own values
--- Assignments/Project1/algo2.py
+++ Assignments/Project1/algo2.py
@@ -1,15 +1,5 @@
 """
-Pseudocode:
-
-create answer array
-
-get the combinedSize of DAArray1 and DAArray2, add them together
-
-sort answer array
-
-then from here remove the elements that do fit within the opposite ends of the schedule array
-then start popping/removing elements that when subtracted dont fit the duration of the meeting
-
+Submission for Cesar Gutierrez
 """
 
 def matchingGroupSchedules(person1_DA, person1_Sch, person2_DA, person2_Sch, duration_of_meeting):
@@ -19,30 +9,71 @@ def matchingGroupSchedules(person1_DA, person1_Sch, person2_DA, person2_Sch, dur
     Daily Activities is the non available times, and Schedules is the complete time a person could meet
     """
 
-    # change both arrays into ints and remove the semicolon
-    # n^2 total I believe for two arrays of size n
+    # change arrays into integers and remove the semicolon
+    # O( a^2 * b^2 * c * d)
     for i in range(len(person1_DA)):
-        person1_DA[i] = int(person1_DA[i].replace(':',''))
-
+        for j in range(len(person1_DA[i])):
+          person1_DA[i][j] = int(person1_DA[i][j].replace(':',''))
     for i in range(len(person2_DA)):
-        person2_DA[i] = int(person2_DA[i].replace(':', ''))
+        for j in range(len(person2_DA[i])):
+          person2_DA[i][j] = int(person2_DA[i][j].replace(':',''))
+    for i in range(len(person1_Sch)):
+      person1_Sch[i] = int(person1_Sch[i].replace(':', ''))
+    for i in range(len(person2_Sch)):
+      person2_Sch[i] = int(person2_Sch[i].replace(':', ''))
 
-    answer = person1_DA + person2_DA
+    # combine the two, person1_DA will be our working array we will manipulate
+    person1_DA += person2_DA
     # n log n sort
-    answer.sort()
+    person1_DA.sort()
+
+    # find the time window that both members are available at the same time
+    person1_Sch[0] = max(person1_Sch[0], person2_Sch[0])
+    person1_Sch[1] = min(person1_Sch[1], person2_Sch[1])
+
+    # removes times that are outside of the members regular schedule
+    for row in person1_DA:
+       if row < person1_Sch:
+          person1_DA.remove(row)
+       if row[0] > person1_Sch[1]:
+          person1_DA.remove(row)
+
+    # create output array
+    answer = []
+
+    # append to output array only the times of the daily activities that meet the duration_of_meeting
+    for i in range(len(person1_DA) - 1):
+        if person1_DA[i][1] < person1_DA[i+1][0]:
+            if (person1_DA[i+1][0] - person1_DA[i][1]) >= duration_of_meeting:
+              answer.append([person1_DA[i][1],person1_DA[i+1][0]])
+
+    # add to the beginning of the output array any free time both could meet that follows the d_of_m & time window schedule
+    if (person1_DA[0][0] - person1_Sch[0]) >= duration_of_meeting:
+       answer.insert(0, [person1_Sch[0], person1_DA[0][0]])
+
+    # add to the end of the output array any free time both could meet that follows the d_of_m & time window schedule
+    if ( person1_Sch[1] - person1_DA[ len(person1_DA) - 1][1] ) >= duration_of_meeting:
+       answer.append([person1_DA[ len(person1_DA) -1 ][1], person1_Sch[1]])
+
+    # format the output array back to string, and add the semicolon in the correct index
+    for i in range(len(answer)):
+       for j in range(len(answer[i])):
+          answer[i][j] = str(answer[i][j])
+          answer[i][j] = (answer[i][j])[:-2] + ':' + (answer[i][j])[-2:]
 
     return answer
+
 
 """
 Test Case
 """
-person1_DA = ['7:00', '8:30',  '12:00', '13:00',  '16:00', '18:00']
-person2_DA = ['9:00', '10:30',  '12:20', '14:30',  '14:00', '15:00', '16:00', '17:00']
+person1_DailyActivity = [['7:00', '8:30'],  ['12:00', '13:00'],  ['16:00', '18:00']]
+person2_DailyActivity = [['9:00', '10:30'],  ['12:20', '14:30'],  ['14:00', '15:00'], ['16:00', '17:00']]
 
-person1_Sch = [9,19]
-person2_Sch = [9,18.5]
+person1_Schedule = ['9:00', '19:00']
+person2_Schedule = ['9:00', '18:30']
 
 duration_of_meeting = 30
 
-result = matchingGroupSchedules(person1_DA, person1_Sch, person2_DA,person2_Sch, duration_of_meeting)
+result = matchingGroupSchedules(person1_DailyActivity, person1_Schedule, person2_DailyActivity,person2_Schedule, duration_of_meeting)
 print(result)
